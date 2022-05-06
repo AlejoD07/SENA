93cee3aec04e0de87c2c4c8e3797997b7fe9d6a6
AlejoD07 <105054024+AlejoD07@users.noreply.github.com>
2022-05-06 07:05:26 -0500

    Create clase.py

---
+++ clase.py
@@ -0,0 +1,26 @@
+import math
+#from pickle import APPEND
+#a=[1,2,3,4,5,6]
+#b=[]
+
+#for i in a:
+    #b.append= math.sqrt(i) 
+    #print(b)
+#-----------------
+#n=int(input("Digite un numero entero:"))
+#a= []
+#for i in range(1,n):
+    #a.append(n**3)
+#print(a)
+#-------------------------
+
+#------------------
+#a=[-1,-2,-3,-4]
+#a=[(x>0)*x for x in a]
+#print("Resultado: ", a)
+#----------------------
+a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,-15,16,17,18,19,20]
+for x in a:
+    b=[(x>0)]
+    
+    print(b)
