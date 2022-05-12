8faab1a66a8791e15346610ca22007ed3db4e03c
AlejoD07 <105054024+AlejoD07@users.noreply.github.com>
2022-05-12 18:03:28 -0500

    Add files via upload

---
+++ Taller9mayo.py
@@ -0,0 +1,44 @@
+#--------- ejercicio 1
+#r=int(input("Digite el lado: "))
+#pi=3.14
+#a=pi*r**2
+#print(a)
+#---------- ejercicio 2
+#l1=float(input("Ingrese lado 1: "))
+#l2=float(input("Ingrese lado 2: "))
+#l3=float(input("Ingrese lado 3: "))
+#l4=float(input("Ingrese lado 4: "))
+#n=l1*l2*l3*l4
+#print(n)
+#----------- ejercicio 3
+#a1=int(input("Ingrese el valor de mesa: "))
+#a2=int(input("Ingrese el valor de silla: "))
+#a3=int(input("Ingrese el valor de reloj: "))
+#n=a1*19/100+a1
+#m=a2*19/100+a2
+#l=a3*19/100+a3
+#s=n+m+l
+#print(n,m,l)
+#print("El valor a pagar por los productos es: ", s)
+#------------ ejercicio 4
+#n=int(input("Digite los numeros a dividir: "))
+#m=int(input("Digite el divisor: "))
+#s=n%m
+#print("El residuo de la division es: ", s)
+#------------ ejercicio 5
+#e1=int(input("Ingrese edad 1: "))
+#e2=int(input("Ingrese edad 2: "))
+#e3=int(input("Ingrese edad 3: "))
+#n=e1+e2+e3
+#l=n/3
+#print(l)
+#----------- ejercicio 6
+#numero=int(input("Ingrese el numero: "))
+#tmpnumero=numero
+#dig=int(0)
+#suma=0
+#while(tmpnumero!=0):
+  #  dig=int(tmpnumero%10)
+  #  suma+=dig
+ #   tmpnumero=int(tmpnumero/10)
+#print("La suma de los digitos de", numero, "es", suma)
